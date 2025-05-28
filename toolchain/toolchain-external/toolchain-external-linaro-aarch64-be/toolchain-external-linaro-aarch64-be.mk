1ac8edcf22469273d6a5c247cca82e64958b1196
Kory Maincent <kory.maincent@bootlin.com>
2025-05-28 13:32:01 +0000

    toolchain-external-linaro*: Add support for license file description
    Add support for Linaro toolchain license description using the
    files in share/info/ as the licenses metadata. This ensures license compliance
    when using external Linaro toolchains.
    
    Signed-off-by: Kory Maincent <kory.maincent@bootlin.com>

--- toolchain/toolchain-external/toolchain-external-linaro-aarch64-be/toolchain-external-linaro-aarch64-be.mk
+++ toolchain/toolchain-external/toolchain-external-linaro-aarch64-be/toolchain-external-linaro-aarch64-be.mk
@@ -13,4 +13,28 @@ else
 TOOLCHAIN_EXTERNAL_LINARO_AARCH64_BE_SOURCE = gcc-linaro-7.3.1-$(TOOLCHAIN_EXTERNAL_LINARO_AARCH64_BE_VERSION)-x86_64_aarch64_be-linux-gnu.tar.xz
 endif
 
+TOOLCHAIN_EXTERNAL_LINARO_AARCH64_BE_LICENSE = multiple
+TOOLCHAIN_EXTERNAL_LINARO_AARCH64_BE_LICENSE_FILES = \
+	share/info/annotate.info \
+	share/info/as.info \
+	share/info/bfd.info \
+	share/info/binutils.info \
+	share/info/cpp.info \
+	share/info/cppinternals.info \
+	share/info/gcc.info \
+	share/info/gccinstall.info \
+	share/info/gccint.info \
+	share/info/gdb.info \
+	share/info/gfortran.info \
+	share/info/gmp.info \
+	share/info/gmp.info-1 \
+	share/info/gmp.info-2 \
+	share/info/gprof.info \
+	share/info/ld.info \
+	share/info/libgomp.info \
+	share/info/libitm.info \
+	share/info/mpc.info \
+	share/info/mpfr.info \
+	share/info/stabs.info
+
 $(eval $(toolchain-external-package))
--- toolchain/toolchain-external/toolchain-external-linaro-arm/toolchain-external-linaro-arm.mk
+++ toolchain/toolchain-external/toolchain-external-linaro-arm/toolchain-external-linaro-arm.mk
@@ -12,5 +12,28 @@ TOOLCHAIN_EXTERNAL_LINARO_ARM_SOURCE = gcc-linaro-7.3.1-$(TOOLCHAIN_EXTERNAL_LIN
 else
 TOOLCHAIN_EXTERNAL_LINARO_ARM_SOURCE = gcc-linaro-7.3.1-$(TOOLCHAIN_EXTERNAL_LINARO_ARM_VERSION)-x86_64_arm-linux-gnueabihf.tar.xz
 endif
+TOOLCHAIN_EXTERNAL_LINARO_ARM_LICENSE = multiple
+TOOLCHAIN_EXTERNAL_LINARO_ARM_LICENSE_FILES = \
+	share/info/annotate.info \
+	share/info/as.info \
+	share/info/bfd.info \
+	share/info/binutils.info \
+	share/info/cpp.info \
+	share/info/cppinternals.info \
+	share/info/gcc.info \
+	share/info/gccinstall.info \
+	share/info/gccint.info \
+	share/info/gdb.info \
+	share/info/gfortran.info \
+	share/info/gmp.info \
+	share/info/gmp.info-1 \
+	share/info/gmp.info-2 \
+	share/info/gprof.info \
+	share/info/ld.info \
+	share/info/libgomp.info \
+	share/info/libitm.info \
+	share/info/mpc.info \
+	share/info/mpfr.info \
+	share/info/stabs.info
 
 $(eval $(toolchain-external-package))
